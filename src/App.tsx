883176b7b704e90e49abf69f51463534b0815464
Anton-Osip <osipchika@gmail.com>
2024-08-14 12:33:39 +0300

    Знакомство с  Error404, Navigate

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,46 @@
 import React, {useState} from 'react';
 import styles from "./components/Site.module.css";
+import {Navigate, Route, Routes} from "react-router-dom";
+import {PageOne} from "./components/pages/PageOne";
+import {PageTwo} from "./components/pages/PageTwo";
+import {PageThree} from "./components/pages/PageThree";
+import {Error404} from "./components/pages/Error404";
 
 
 function App() {
     return (
         <div>
-            <div className={styles.header}><h1>HEADER</h1></div>
-            <div className={styles.body}>
-                <div className={styles.nav}>
+            <div className = {styles.header}><h1>HEADER</h1></div>
+            <div className = {styles.body}>
+                <div className = {styles.nav}>
                     Здесь будет навигация
                 </div>
-                <div className={styles.content}>
-                    Здесь будут кроссовки
+                <div className = {styles.content}>
+
+                    <Routes>
+                        <Route
+                            element = {<Navigate to = {"/page1"}/>}
+                            path = "/"/>
+                        <Route
+                            element = {<PageOne/>}
+                            path = "/page1"/>
+                        <Route
+                            element = {<PageTwo/>}
+                            path = "/page2"/>
+                        <Route
+                            element = {<PageThree/>}
+                            path = "/page3"/>
+                        <Route
+                            element = {<Error404/>}
+                            path = "/error404"/>
+                        <Route
+                            element = {<Navigate to = {"/error404"}/>}
+                            path = "/*"/>
+
+                    </Routes>
                 </div>
             </div>
-            <div className={styles.footer}>abibas 2023</div>
+            <div className = {styles.footer}>abibas 2023</div>
         </div>
     );
 }
